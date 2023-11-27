0846e528c2db787197386b609751eaecfad8b14a
victoryejike <victoryejike@gmail.com>
2023-11-27 01:47:17 +0100

    added a proxy object for updating the notification object

--- notification-page/src/components/Body.tsx
+++ notification-page/src/components/Body.tsx
@@ -1,11 +1,11 @@
 import Card from "./Card";
-import { anotherProxy } from "../constants/Mock";
+import { Notification } from "../constants/Mock";
 import { NotificationType } from "../constants/Mock";
 
 const Body: React.FC = () => {
   return (
     <div className="pb-3">
-      {anotherProxy.map((notice: NotificationType) => (
+      {Notification.map((notice: NotificationType) => (
         <Card key={notice.profileName} data={notice} />
       ))}
     </div>
